e234c4ffd89f59a543822b0b02d57da1d4414dff
anilercan09 <52035643+anilercan09@users.noreply.github.com>
2022-11-05 14:02:37 +0300

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,22 +1,10 @@
-from cmath import log
-import numpy as np
 import cv2
-
-def chunkIt(seq, num):
-    avg = 96 / float(num)
-    out = []
-    last = 0.0
-
-    while last < 96:
-        out.append(seq[int(last):int(last + avg)])
-        last += avg
-
-    return out
+import numpy as np
+from cmath import log
 
 def divergenceD(q,s,k_means):
     res = 0
-    for i in range(k_means):
-        
+    for i in range(k_means): 
         res += q[i]*log((q[i] / s[i]),10)
         
     return res
@@ -24,95 +12,116 @@ def divergenceD(q,s,k_means):
 def divergenceJSD(q,s,k_means):
     res = (1/2) * divergenceD(q,np.divide( np.add(q,s), 2 ),k_means) + (1/2) * divergenceD(s, np.divide( np.add(q,s), 2 ), k_means)
     return res
-    
 
-def perChannel(k_means, queryNo,source_cache,lines):
-
+def perChannel(source_cache, lines, queryNo, grid_dimension, k_means):
     length = len(lines)
-
     correct_comparision=0
-    for l1 in lines:
 
-        min_divergence = 9999999999
-        closest_picture_name=""
-        query_image_path = "dataset/query_"+queryNo+"/"+l1
+    for x in range(length):
+        #-------------------------------------------------------- FOR EACH IMAGE
+        query_image_path = "dataset/query_"+queryNo+"/"+lines[x]
         query_image = np.array(cv2.imread(query_image_path))
 
-        query_blue_column = np.zeros(k_means)
-        query_green_column = np.zeros(k_means)
-        query_red_column = np.zeros(k_means)
+        query_image = np.split(query_image,grid_dimension)
+        for i in range(len(query_image)):
+            query_image[i] = np.hsplit(query_image[i],grid_dimension)
 
-        for i in range(96):
-            for j in range(96):
-                query_blue_column[int(query_image[i][j][0] / (256/k_means))]+=1
-                query_green_column[int(query_image[i][j][1] / (256/k_means))]+=1
-                query_red_column[int(query_image[i][j][2] / (256/k_means))]+=1
+        
 
-        for i in range(k_means):
-            query_blue_column[i] = query_blue_column[i] / 9216
-            if(query_blue_column[i] == 0):
-                query_blue_column[i]+=1/9216
-            query_green_column[i] = query_green_column[i] / 9216
-            if(query_green_column[i] == 0):
-                query_green_column[i]+=1/9216
-            query_red_column[i] = query_red_column[i] / 9216
-            if(query_red_column[i] == 0):
-                query_red_column[i]+=1/9216
+        
+        
+        query_cache = [ [ [] for b in range(grid_dimension) ] for a in range(grid_dimension)]
+        
+        for i in range(grid_dimension):
+            for j in range(grid_dimension):
+                #-------------------------------------------------- FOR EACH GRID
+
+                query_blue_column = np.zeros(k_means)
+                query_green_column = np.zeros(k_means)
+                query_red_column = np.zeros(k_means)
+
+                for k in range(int(96/grid_dimension)):
+                    for l in range(int(96/grid_dimension)):
+                        query_blue_column[int(query_image[i][j][k][l][0] / (256/k_means))]+=1
+                        query_green_column[int(query_image[i][j][k][l][1] / (256/k_means))]+=1
+                        query_red_column[int(query_image[i][j][k][l][2] / (256/k_means))]+=1
+
+                for a in range(k_means):
+                    query_blue_column[a] = query_blue_column[a] / 9216
+                    if(query_blue_column[a] == 0):
+                        query_blue_column[a]+=1/9216
+                    query_green_column[a] = query_green_column[a] / 9216
+                    if(query_green_column[a] == 0):
+                        query_green_column[a]+=1/9216
+                    query_red_column[a] = query_red_column[a] / 9216
+                    if(query_red_column[a] == 0):
+                        query_red_column[a]+=1/9216
                 
-        for i in range(length):
-            divergence = (divergenceJSD(query_blue_column, source_cache[i][0],k_means) + divergenceJSD(query_green_column, source_cache[i][1],k_means) + divergenceJSD(query_red_column, source_cache[i][2],k_means))
-            
+                query_cache[i][j] = [query_blue_column, query_green_column, query_red_column]
+        #--------------------------------------------------------- COMPARE THE IMAGE WITH SOURCE_CACHE       
+        
+        name_of_the_similar_image=""
+        min_divergence = 999999
+        for i in range(length):   
+            divergence = 0
+            for j in range(grid_dimension):  
+                for k in range(grid_dimension):
+                    divergence += ( divergenceJSD(query_cache[j][k][0],source_cache[i][j][k][0],k_means) + divergenceJSD(query_cache[j][k][1],source_cache[i][j][k][1],k_means) + divergenceJSD(query_cache[j][k][2],source_cache[i][j][k][2],k_means) ) / (3*(grid_dimension**2))
             if(divergence < min_divergence):
                 min_divergence = divergence
-                closest_picture_name = lines[i]
-        if(closest_picture_name == l1):
-            correct_comparision += 1
-    print(correct_comparision)
-if __name__=="__main__":
+                name_of_the_similar_image = lines[i]
 
+        if(name_of_the_similar_image == lines[x]):
+            correct_comparision += 1    
+
+    print(correct_comparision)      
+               
+                    
+                    
+
+if __name__=="__main__":
     queryNo = input("Enter query number(1,2,3) which is to be compared with support queries: ") 
+    grid_dimension = int( input("Enter grid dimension (square grid): ") )
     k_means = int( input("Enter k_means (must divide 256): ") )
 
     with open("dataset/InstanceNames.txt") as file:
         lines = [line.rstrip() for line in file]
 
-    
     length = len(lines)
-    source_cache = [[] for i in range(length) ]
-
+    source_cache = [ [ [ [] for c in range(grid_dimension) ] for b in range(grid_dimension) ] for a in range(length) ]
     for x in range(length):
         source_image_path = "dataset/support_96/"+lines[x]
         source_image = np.array(cv2.imread(source_image_path))
+        #----------------------------------------------------------------------- MAKE GRID    
+        source_image = np.split(source_image,grid_dimension)
 
-        source_blue_column = np.zeros(k_means)
-        source_green_column = np.zeros(k_means)
-        source_red_column = np.zeros(k_means)
-
-        for i in range(96):
-            for j in range(96):
-                source_blue_column[int(source_image[i][j][0] / (256/k_means))]+=1
-                source_green_column[int(source_image[i][j][1] / (256/k_means))]+=1
-                source_red_column[int(source_image[i][j][2] / (256/k_means))]+=1
-
-        for i in range(k_means):
-            source_blue_column[i] = source_blue_column[i] / 9216
-            if(source_blue_column[i] == 0):
-                source_blue_column[i]+=1/9216
-            source_green_column[i] = source_green_column[i] / 9216
-            if(source_green_column[i] == 0):
-                source_green_column[i]+=1/9216
-            source_red_column[i] = source_red_column[i] / 9216
-            if(source_red_column[i] == 0):
-                source_red_column[i]+=1/9216
-        source_cache[x] = [source_blue_column, source_green_column, source_red_column]
-    
-    """for i in range(length):
-        for j in range(3):
-            for k in range(k_means):
-                if(source_cache[i][j][k] == 0):
-                    print(i,j,k)
-    """
+        for i in range(len(source_image)):
+            source_image[i] = np.hsplit(source_image[i],grid_dimension)
         
-    #print(source_cache[8][0][3])
-    perChannel(k_means,queryNo, source_cache,lines)
-    
+        for i in range(grid_dimension):
+            for j in range(grid_dimension):
+                #----------------------------------------------------------------------- FOR EACH GRID    
+                source_blue_column = np.zeros(k_means)
+                source_green_column = np.zeros(k_means)
+                source_red_column = np.zeros(k_means)
+                
+                for k in range(int(96/grid_dimension)):
+                    for l in range(int(96/grid_dimension)):
+                        source_blue_column[int(source_image[i][j][k][l][0] / (256/k_means))]+=1
+                        source_green_column[int(source_image[i][j][k][l][1] / (256/k_means))]+=1
+                        source_red_column[int(source_image[i][j][k][l][2] / (256/k_means))]+=1
+
+                for a in range(k_means):
+                    source_blue_column[a] = source_blue_column[a] / 9216
+                    if(source_blue_column[a] == 0):
+                        source_blue_column[a]+=1/9216
+                    source_green_column[a] = source_green_column[a] / 9216
+                    if(source_green_column[a] == 0):
+                        source_green_column[a]+=1/9216
+                    source_red_column[a] = source_red_column[a] / 9216
+                    if(source_red_column[a] == 0):
+                        source_red_column[a]+=1/9216
+                source_cache[x][i][j] = [source_blue_column, source_green_column, source_red_column]
+            
+
+    perChannel(source_cache, lines, queryNo, grid_dimension, k_means)
